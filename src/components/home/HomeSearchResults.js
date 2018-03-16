8b3ab24771fb6499058df354c0f200e9aadced47
Patrick Nærvig Wøhlk <patrick.naervig.wohlk@LEGO.com>
2018-03-16 15:33:16 +0100

    Removed references´

--- src/components/home/HomeSearchResults.js
+++ src/components/home/HomeSearchResults.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import {PropTypes} from 'prop-types';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
 import moment from 'moment';
 import {Link} from 'react-router';
 
